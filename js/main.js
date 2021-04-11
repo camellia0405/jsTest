7b110c05ea73af8944d9f096f7da2bc56760e80b
umehara2jp <u.natsu.co@gmail.com>
2021-04-11 13:30:30 +0900

    ファイル追加

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+let HeipElem = document.getElementById("Help");
+let Help ="表示できているかな？";
+
+HeipElem.innerHTML =Help;
+
+console.log("コンソールに表示");
+
+let FamilyName ="yamada";
+let LastName ="hanako";
+
+console.log(FamilyName + LastName);
+console.log(1 + 2 + 3);
+console.log("鈴木" + 3);
